903715364aee38eacd5ce0995f7ecf1c6a9757f3
Mrgrimace <35194818+Mrgrimace@users.noreply.github.com>
2018-01-31 15:42:18 +0100

    Avec le son lors de l'impact
    Script ImpactSurLePlayer

---
+++ Hackaton/Assets/Scripts/Impact/DestructionProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		
+		if (col.gameObject.name == "Projectile(Clone)") {
+			Destroy (col.gameObject);
+		}
+	}
+}
