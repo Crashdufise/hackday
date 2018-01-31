3854310df57697afc4be4dd95c1a6986f0a55adb
Agathe <aga.guenot@gmail.com>
2018-01-31 18:11:39 +0100

    Script BigHands

---
+++ Hackaton/Assets/Scripts/Projectiles/BigHand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigHand : MonoBehaviour {
+
+	public Valve.VR.InteractionSystem.Hand hand;
+
+	public float min = 0.1f;
+	public float max = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hand.GetStandardInteractionButton ()) {
+			transform.localScale *= 1.05f;
+			transform.localScale = Mathf.Min (transform.localScale, max);
+		} else {
+			transform.localScale *= 0.9f;
+			transform.localScale = Mathf.Max (transform.localScale, min);
+		}
+	}
+}
