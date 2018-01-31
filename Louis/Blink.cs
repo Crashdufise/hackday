663275c04a324675521c8dfde10196cb5011f5d6
LLN <Crashdufise@gmail.com>
2018-01-31 16:47:13 +0100

    Gestion du menu
    Scripts permettant le clignottement et le changement de scène

---
+++ Louis/Blink.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+    public float delay;
+
+	// Use this for initialization
+	void Start () {
+        delay = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        delay += 1;
+        if (delay > 90)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            delay = 0;
+        }
+        if (delay == 45)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
